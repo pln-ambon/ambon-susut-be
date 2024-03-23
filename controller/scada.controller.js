201aedb136e2f4811a500b630cce7281dce6bfe9
ariftoteles <arif.santoso4@bithealth.com>
2024-03-23 15:34:31 +0700

    test api auth

--- controller/scada.controller.js
+++ controller/scada.controller.js
@@ -5,11 +5,12 @@ const {
 
 async function getAllUnit(req, res) {
   try {
+    console.log("<< get all unit");
     const units = await getAllScadaUnit()
 
     res.status(200).json(units)
   } catch (error) {
-    res.send(error?.code || 500 ).json(error)
+    res.status(error?.code || 500 ).json(error)
   }
 }
 
