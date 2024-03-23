54b55c767a263615ab04b1f4813a24148bb6d6bb
ariftoteles <arif.santoso4@bithealth.com>
2024-03-23 16:11:52 +0700

    test api auth

--- model/scada_unit.model.js
+++ model/scada_unit.model.js
@@ -18,13 +18,21 @@ async function getAllScadaUnitMeter() {
     const pool = await sql.connect(sqlConfig);
     const result = await pool.request()
       .query(`
-        SELECT A.*, B.*
-        FROM SCADA_UNIT A
+      SELECT A.*,
+        B.*
+      FROM SCADA_UNIT A
         OUTER APPLY (
-            SELECT TOP 1 *
-            FROM SCADA_METER_2 B
-            WHERE B.unit_id = A.unit_id
-            ORDER BY B.id DESC
+          SELECT *
+          FROM (
+              SELECT *,
+                ROW_NUMBER() OVER (
+                  PARTITION BY unit_subname
+                  ORDER BY id DESC
+                ) AS row_num
+              FROM SCADA_METER_2 B
+              WHERE B.unit_id = A.unit_id
+            ) AS C
+          WHERE C.row_num = 1
         ) AS B;
       `);
 
