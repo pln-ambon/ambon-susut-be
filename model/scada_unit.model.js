1ed6a41392ff4ae2402514dd20c046f854bc5595
ariftoteles <arif.santoso4@bithealth.com>
2024-03-23 15:36:22 +0700

    test api auth

--- model/scada_unit.model.js
+++ model/scada_unit.model.js
@@ -1,10 +1,13 @@
 const sql = require('mssql');
+const { sqlConfig } =require("../config")
 
 async function getAllScadaUnit() {
   try {
     const pool = await sql.connect(sqlConfig);
     const result = await pool.request()
       .query('SELECT * FROM SCADA_UNIT');
+
+    console.log(result, "<< result");
 
     return result.recordset;
   } catch (error) {
