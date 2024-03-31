ca37961adfba47dbad93312ac61eee052ee60f89
ariftoteles <arif.santoso4@bithealth.com>
2024-03-31 09:26:34 +0700

    add total

--- controller/scada.controller.js
+++ controller/scada.controller.js
@@ -312,8 +312,8 @@ async function getLatest24HourEvery5Minute(req, res) {
   try {
 
     let startOfHour = moment().utc().startOf('hour').set({ minute: 0, second: 0 }); // Mulai jam ini, atur menit dan detik menjadi 00:00
-    let lastDay = startOfHour.subtract(1, 'day')
-    let formattedStartTime = lastDay.format('YYYY-MM-DD HH:mm:ss'); // Format waktu sesuai kebutuhan SQL
+    let lastDay = startOfHour.utc().subtract(1, 'day')
+    let formattedStartTime = lastDay.utc().format('YYYY-MM-DD HH:mm:ss'); // Format waktu sesuai kebutuhan SQL
     const startTime = new Date(formattedStartTime) 
 
     const result = {}
